effad82ce3e6de3c5f13bb1aa9c24464d547d895
Saurabh Shiradkar <72598771+saurabhshiradkar@users.noreply.github.com>
2024-11-22 14:45:59 +0530

    Jasper Report With SpringBoot

---
+++ src/main/java/com/sauranet/reports/controller/ReportController.java
@@ -0,0 +1,59 @@
+package com.sauranet.reports.controller;
+
+import com.sauranet.reports.entities.Student;
+import com.sauranet.reports.services.StudentService;
+import com.sauranet.reports.utils.ReportUtil;
+import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/reports")
+@RequiredArgsConstructor
+public class ReportController {
+
+    private final StudentService studentService;
+
+
+    @GetMapping("/student/{rollNumber}")
+    public ResponseEntity<byte[]> generateStudentReport(@PathVariable(name = "rollNumber") Integer rollNumber) throws IOException {
+        try {
+            // Fetch student with given roll number
+            List<Student> students = null;
+            if (rollNumber != null) {
+                students = List.of(studentService.getStudentById(rollNumber));
+            } else {
+                students.add(new Student());
+            }
+
+            // Path to the compiled .jasper file
+            String jasperPath = new File("src/main/resources/reports/StudentScoreCard.jasper").getAbsolutePath();
+
+            // Parameters for the report
+            HashMap<String, Object> parameters = new HashMap<>();
+            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(students.get(0).getScoreCards());
+            parameters.put("TABLE_DATA_SOURCE", dataSource);
+
+            // Generate the PDF report
+            byte[] pdfData = ReportUtil.generateReport(students, jasperPath, parameters);
+
+            // Return the PDF as a response
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=student_report.pdf")
+                    .header(HttpHeaders.CONTENT_TYPE, "application/pdf")
+                    .body(pdfData);
+
+        } catch (JRException e) {
+            throw new RuntimeException("Error generating report", e);
+        }
+    }
+}
+
